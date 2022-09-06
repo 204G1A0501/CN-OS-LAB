338dfd77fc0d70570f504fc2ad381a28182f41f0
Abhigna-M <94513749+Abhigna-M@users.noreply.github.com>
2022-09-06 11:56:28 +0530

    m.abhigna Created fcfs.c

---
+++ fcfs.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: m.abhigna
+   Roll No: 204G1A0501
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
